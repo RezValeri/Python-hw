3b4fa99ec2ebe0b7ff9bc17bfadca83f72770238
RezValeri <48764084+RezValeri@users.noreply.github.com>
2019-10-29 13:31:51 +0300

    Comments from Lera

--- Azat/mycsv.py
+++ Azat/mycsv.py
@@ -1,38 +1,42 @@
 #!/usr/bin/python
 
 import os
-
+#Here Azat import os module
 def read_csv(filename,delimeter:str) -> list:
+#As I understood, he wrot function and make it write results as a list
 	table = []
+#Making an empty list
 	if os.path.exists(filename):
-
-		
+#Doing it for writing an error if input file doesn't exist
 		with open(filename) as file:
+#Open file for reading
 			for line in file:
 				lst_line = []
+#Making list in a loop
 				quote = 0
+#For not spliting "0,5" in example
 				for i in line:
-					# Treat lines by symbols
+#Treat lines by symbols
 					if i == delimeter:
 						if quote:
-						# if we open quote ", we add delimeter symbol, not whitespace  
+#If we open quote ", we add delimeter symbol, not whitespace  
 							lst_line.append(delimeter)
 						else:
-					# substitute delimeter by spaces
+#Substitute delimeter by spaces
 							lst_line.append(' ')
 
 					elif i == '"':
-					# opening and closing of quotes
+#If he has '"0,5"'
 						quote = (quote + 1) % 2
 					else:
-					# If i is not delimeter or quote, we just append it
+#If i is not delimeter or quote, he just append it
 						lst_line.append(i)
-				# join and split by spaces the result string and add result list in 'table' 
+#Join and split by spaces the result string and add result list in 'table' 
 				table.append(''.join(lst_line).split())
 	else:
 		print("Error, such file doesn't exist")
 
-# P.S. I suppose throw of exception instead printing is better approach
+#(From Azat) P.S. I suppose throw of exception instead printing is better approach
 	
 	return table 
 
@@ -44,8 +48,10 @@ def write_csv(filename,data,delimeter:str):
 # What is incorrect call for write_csv?
 
 	with open(filename,'w') as file:
+#Opening file for writing
 		for row in data:
 			file.write(delimeter.join(row)+"\n")
+#Joining adelinetor and making split for next row
 
 
 
